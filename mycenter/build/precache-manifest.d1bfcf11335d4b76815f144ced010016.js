b9ad65883d43bda63f3fbad077526d3d9e8e9007
Nicolas Passaglia <nicopassaglia@hotmail.com>
2019-12-16 16:56:46 -0300

    Build

---
+++ mycenter/build/precache-manifest.d1bfcf11335d4b76815f144ced010016.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2753cf0688411b97e064a12232f4cce7",
+    "url": "/static/media/logo-light.2753cf06.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c0bf1c63e37f38617fea",
+    "url": "/static/js/main.2a945a5f.chunk.js"
+  },
+  {
+    "revision": "e33d0bd7c245101005f8",
+    "url": "/static/js/2.f8cb4d63.chunk.js"
+  },
+  {
+    "revision": "c0bf1c63e37f38617fea",
+    "url": "/static/css/main.e34ab79f.chunk.css"
+  },
+  {
+    "revision": "ca4f901b7462df2f70fee92a12f06e2e",
+    "url": "/index.html"
+  }
+];
